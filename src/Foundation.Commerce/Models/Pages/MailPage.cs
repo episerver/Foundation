70153ac1d8ead7315ea9997eb83aeafb69874c69
tanvu-epi <tan.vu@episerver>
2019-10-16 18:13:46 +0700

    Render Heading BlogListPage for editable in editmode

--- src/Foundation.Commerce/Models/Pages/MailPage.cs
+++ src/Foundation.Commerce/Models/Pages/MailPage.cs
@@ -8,6 +8,7 @@ namespace Foundation.Cms.Pages
         GUID = "73bc5587-eef3-4844-be9d-0c90d081e2e4",
         Description = "The template mail page.",
         GroupName = CmsTabNames.Account)]
+    [ImageUrl("~/assets/icons/cms/pages/CMS-icon-page-26.png")]
     public class MailPage : MailBasePage
     {
     }
