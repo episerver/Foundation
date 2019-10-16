fb044cdc5021312531dffcd8d3595fd0575e2c19
tanvu-epi <tan.vu@episerver>
2019-10-16 14:51:11 +0700

    #59: issues relate to Catalog Detail, Reset password

---
+++ src/Foundation.Commerce/Models/Pages/MailPage.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using Foundation.Commerce.Models.Pages;
+
+namespace Foundation.Cms.Pages
+{
+    [ContentType(DisplayName = "Mail Page",
+        GUID = "73bc5587-eef3-4844-be9d-0c90d081e2e4",
+        Description = "The template mail page.",
+        GroupName = CmsTabNames.Account)]
+    public class MailPage : MailBasePage
+    {
+    }
+}
