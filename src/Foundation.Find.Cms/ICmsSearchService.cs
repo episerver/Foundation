a639a6caf9bb6f669ce956a97c65d0474a36cd2c
tanvu-epi <56108258+tanvu-epi@users.noreply.github.com>
2019-10-18 23:06:06 +0700

    #33: Apply Geta.EPiCategories for Recent Page Category Recommendation Block (#66)
    * #33: Apply Geta.EPiCategories for Recent Page Category Recommendation Block

--- src/Foundation.Find.Cms/ICmsSearchService.cs
+++ src/Foundation.Find.Cms/ICmsSearchService.cs
@@ -1,5 +1,9 @@
-﻿using EPiServer.Find.UnifiedSearch;
+﻿using EPiServer.Core;
+using EPiServer.Find;
+using EPiServer.Find.UnifiedSearch;
 using Foundation.Find.Cms.ViewModels;
+using Geta.EpiCategories;
+using System.Collections.Generic;
 
 namespace Foundation.Find.Cms
 {
@@ -7,5 +11,6 @@ namespace Foundation.Find.Cms
     {
         ContentSearchViewModel SearchContent(CmsFilterOptionViewModel filterOptions);
         CategorySearchResults SearchByCategory(Pagination pagination);
+        ITypeSearch<T> FilterByCategories<T>(ITypeSearch<T> query, IEnumerable<ContentReference> categories) where T : ICategorizableContent;
     }
 }
