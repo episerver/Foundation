affc088196867a6c2bc01ef7c4d5f3d583ad178c
Ngoc Nguyen <ngoc.nguyen@episerver.com>
2019-10-25 16:34:30 +0700

    #105: Form Elements Enhancements

---
+++ src/Foundation/ClientResources/Scripts/MenuChildItems.js
@@ -0,0 +1,24 @@
+﻿define([
+    "dojo/_base/array",
+    "dojo/_base/declare",
+    "dojo/_base/lang",
+    "epi-cms/contentediting/editors/CollectionEditor",
+    "foundation/MenuChildItems"
+],
+    function (
+        array,
+        declare,
+        lang,
+        CollectionEditor
+    ) {
+        return declare([CollectionEditor], {
+            _getGridDefinition: function () {
+                var result = this.inherited(arguments);
+                //Override: Showing the name of the child items, not [object Object]
+                result.listCategories.formatter = function (values) {
+                    return values.map(msc => msc.text).join();
+                };
+                return result;
+            }
+        });
+    });
