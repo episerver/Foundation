ac60b598d110775bdcc1f902a767ee3edf1cc378
tanvu-epi <tan.vu@episerver>
2019-10-25 14:32:10 +0700

    103: remove BlogListBlock and update BlogListPage

--- src/Foundation.Cms/ViewModels/BlogListPageViewModel.cs
+++ src/Foundation.Cms/ViewModels/BlogListPageViewModel.cs
@@ -1,12 +1,23 @@
-﻿using Foundation.Cms.Pages;
+﻿using EPiServer.Core;
+using Foundation.Cms.Pages;
 using System.Collections.Generic;
 
 namespace Foundation.Cms.ViewModels
 {
     public class BlogListPageViewModel : ContentViewModel<BlogListPage>
     {
-        public BlogListPageViewModel(BlogListPage currentPage) : base(currentPage) { }
+        public BlogListPageViewModel(BlogListPage currentPage) : base(currentPage)
+        {
+            Heading = currentPage.Heading;
+            ShowIntroduction = currentPage.IncludeTeaserText;
+            ShowPublishDate = currentPage.IncludePublishDate;
+        }
 
         public List<KeyValuePair<string, string>> SubNavigation { get; set; }
+        public string Heading { get; set; }
+        public IEnumerable<PageData> Blogs { get; set; }
+        public bool ShowIntroduction { get; set; }
+        public bool ShowPublishDate { get; set; }
+        public PagingInfo PagingInfo { get; set; }
     }
 }
