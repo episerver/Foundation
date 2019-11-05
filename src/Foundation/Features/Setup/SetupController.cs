cddedd40e41b564654c083d3785860cdfa076f17
Mark Hall <mark.hall@episerver.com>
2019-11-05 10:14:05 -0800

    Update packages, add recommendation block

--- src/Foundation/Features/Setup/SetupController.cs
+++ src/Foundation/Features/Setup/SetupController.cs
@@ -132,7 +132,12 @@ namespace Foundation.Features.Setup
 
             var searchManager = new SearchManager(Mediachase.Commerce.Core.AppContext.Current.ApplicationName);
             searchManager.BuildIndex(true);
-            RunIndexJob();
+
+            var config = EPiServer.Find.Configuration.GetConfiguration();
+            if (!config.ServiceUrl.Equals("https://es-us-api01.episerver.com/9IKGqgMZaTD9KP4Op3ygsVB6JeJzR0N6") && !config.DefaultIndex.Equals("episerverab_index55794"))
+            {
+                RunIndexJob();
+            }
 
             return Redirect("/");
         }
