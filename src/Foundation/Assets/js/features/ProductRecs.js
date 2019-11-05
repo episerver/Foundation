641a9862b03196422c517f07bf7d947de398f8ff
Mark Hall <mark.hall@episerver.com>
2019-11-05 10:14:05 -0800

    Update packages, add recommendation block

---
+++ src/Foundation/Assets/js/features/ProductRecs.js
@@ -0,0 +1,21 @@
+﻿class ProductRecs {
+    getRecommendations(widget, numberOfRecs, name, value) {
+        let url = '/WidgetBlock/GetRecommendations';
+        let data = {
+            widgetType: widget,
+            numberOfRecs: numberOfRecs,
+            name: name,
+            value: value
+        };
+        axios.post(url, data)
+            .then(function (result) {
+                $('.recommendationwidgetblock').html(result.data);
+            })
+            .catch(function (error) {
+                notification.Error(error);
+            })
+            .finally(function () {
+                $('body>.loading-box').hide();
+            });
+    }
+}
