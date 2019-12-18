3a0a9a0f8505940b93a42094e1e32b03ce412342
tanvu-epi <tan.vu@episerver.com>
2019-12-18 16:31:06 +0700

    #246: Add description to OrderSearchBlock

--- src/Foundation.Commerce/Models/Blocks/OrderSearchBlock.cs
+++ src/Foundation.Commerce/Models/Blocks/OrderSearchBlock.cs
@@ -7,7 +7,9 @@ using Foundation.Cms.Blocks;
 
 namespace Foundation.Commerce.Models.Blocks
 {
-    [ContentType(DisplayName = "Order Search Block", GUID = "dd74d77f-3dce-4956-87fc-39bdbeebaf9c", Description = "")]
+    [ContentType(DisplayName = "Order Search Block", 
+        GUID = "dd74d77f-3dce-4956-87fc-39bdbeebaf9c", 
+        Description = "A block that allows to search/filter on orders")]
     [ImageUrl("~/assets/icons/cms/blocks/CMS-icon-block-33.png")]
     public class OrderSearchBlock : FoundationBlockData
     {
