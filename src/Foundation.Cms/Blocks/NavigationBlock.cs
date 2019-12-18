e21de3e31ff72ff1ec5c19cc549bc53f5a978000
Ngoc Nguyen <ngoc.nguyen@episerver.com>
2019-12-18 15:53:20 +0700

    #256 Fix unable to load component and update block descriptions

--- src/Foundation.Cms/Blocks/NavigationBlock.cs
+++ src/Foundation.Cms/Blocks/NavigationBlock.cs
@@ -7,6 +7,7 @@ namespace Foundation.Cms.Blocks
 {
     [ContentType(DisplayName = "Navigation Block",
         GUID = "7C53F707-C932-4FDD-A654-37FF2A1258EB",
+        Description = "Render normal left/right navigation structures",
         GroupName = CmsGroupNames.Content)]
     [SiteImageUrl("~/assets/icons/cms/blocks/CMS-icon-block-30.png")]
     public class NavigationBlock : FoundationBlockData
