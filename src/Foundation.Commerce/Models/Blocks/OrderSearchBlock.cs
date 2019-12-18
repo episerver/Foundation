1a30c0cf900dbb09738a9dbe1b30e182c34584d3
tanvu-epi <tan.vu@episerver.com>
2019-12-18 16:35:48 +0700

    add GroupName to OrderSearchBlock

--- src/Foundation.Commerce/Models/Blocks/OrderSearchBlock.cs
+++ src/Foundation.Commerce/Models/Blocks/OrderSearchBlock.cs
@@ -9,7 +9,8 @@ namespace Foundation.Commerce.Models.Blocks
 {
     [ContentType(DisplayName = "Order Search Block", 
         GUID = "dd74d77f-3dce-4956-87fc-39bdbeebaf9c", 
-        Description = "A block that allows to search/filter on orders")]
+        Description = "A block that allows to search/filter on orders",
+        GroupName = CommerceGroupNames.Commerce)]
     [ImageUrl("~/assets/icons/cms/blocks/CMS-icon-block-33.png")]
     public class OrderSearchBlock : FoundationBlockData
     {
