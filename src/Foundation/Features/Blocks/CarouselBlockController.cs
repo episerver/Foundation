a23f4fbc58db266ded90b6abcdaa34c0c0aae4ec
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2019-12-20 16:52:04 +0700

    update carousel
    #267 using ContentArea for personalization

--- src/Foundation/Features/Blocks/CarouselBlockController.cs
+++ src/Foundation/Features/Blocks/CarouselBlockController.cs
@@ -1,4 +1,5 @@
 ﻿using EPiServer;
+using EPiServer.Core;
 using EPiServer.Framework.DataAnnotations;
 using EPiServer.Globalization;
 using EPiServer.Web.Mvc;
@@ -6,6 +7,7 @@ using Foundation.Cms.Blocks;
 using Foundation.Cms.Media;
 using Foundation.Cms.ViewModels;
 using Foundation.Cms.ViewModels.Blocks;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Foundation.Features.Blocks
@@ -14,38 +16,40 @@ namespace Foundation.Features.Blocks
     public class CarouselBlockController : BlockController<CarouselBlock>
     {
         private readonly IContentLoader _contentLoader;
-        private readonly LanguageResolver _languageResolver;
 
-        public CarouselBlockController(IContentLoader contentLoader, LanguageResolver languageResolver)
+        public CarouselBlockController(IContentLoader contentLoader)
         {
             _contentLoader = contentLoader;
-            _languageResolver = languageResolver;
         }
 
         public override ActionResult Index(CarouselBlock currentBlock)
         {
             var model = new CarouselBlockViewModel(currentBlock);
 
-            foreach (var item in _contentLoader.GetItems(currentBlock.CarouselItems, _languageResolver.GetPreferredCulture()))
+            if (currentBlock.CarouselItems.Items.Any())
             {
-                if (item.GetOriginalType().Equals(typeof(ImageMediaData)))
+                foreach (var contentAreaItem in currentBlock.CarouselItems.FilteredItems)
                 {
-                    var image = _contentLoader.Get<ImageMediaData>(item.ContentLink);
-                    var carouselImage = new CarouselImage()
-                    {
-                        Heading = image.Title,
-                        Description = image.Description,
-                        Image = image.ContentLink
-                    };
+                    var carouselItem = _contentLoader.Get<IContentData>(contentAreaItem.ContentLink);
 
-                    model.Items.Add(new CarouselItem() { CarouselImage = carouselImage });
-                }
-                else if (item.GetOriginalType().Equals(typeof(HeroBlock)))
-                {
-                    var heroBlock = _contentLoader.Get<HeroBlock>(item.ContentLink);
-                    model.Items.Add(new CarouselItem() { HeroBlock = new BlockViewModel<HeroBlock>(heroBlock) });
+                    if (carouselItem is ImageMediaData)
+                    {
+                        var carouselImage = new CarouselImage()
+                        {
+                            Heading = ((ImageMediaData)carouselItem).Title,
+                            Description = ((ImageMediaData)carouselItem).Description,
+                            Image = ((ImageMediaData)carouselItem).ContentLink
+                        };
+
+                        model.Items.Add(new CarouselItem() { CarouselImage = carouselImage });
+                    }
+                    else if (carouselItem is HeroBlock)
+                    {
+                        model.Items.Add(new CarouselItem() { HeroBlock = new BlockViewModel<HeroBlock>((HeroBlock)carouselItem) });
+                    }
                 }
             }
+
             return PartialView("~/Features/Blocks/Views/CarouselBlock.cshtml", model);
         }
     }
