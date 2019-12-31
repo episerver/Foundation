f50c19c62b60bda4b15ff2923f9fea20cb145923
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2019-12-31 15:31:13 +0700

    create Product Hero Block type
    #279

---
+++ src/Foundation.Commerce/ViewModels/ProductHeroBlockViewModel.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Core;
+using Foundation.Cms.ViewModels;
+using Foundation.Commerce.Blocks;
+
+namespace Foundation.Commerce.ViewModels
+{
+    public class ProductHeroBlockViewModel : BlockViewModel<ProductHeroBlock>
+    {
+        public ProductHeroBlockViewModel(ProductHeroBlock currentBlock) : base(currentBlock)
+        {
+        }
+
+        public string ImageUrl { get; set; }
+        public string ImagePosition { get; set; }
+        public string BlockPaddings { get; set; }
+        public string CalloutPaddings { get; set; }
+    }
+}
