c2f8a0f43eb33e2ebf9d3b832cd2408a3446f177
Ngoc Nguyen <ngoc.nguyen@episerver.com>
2020-01-10 10:34:26 +0700

    Use constructor injection

--- src/Foundation.Commerce/Models/EditorDescriptors/BrandSelectionFactory.cs
+++ src/Foundation.Commerce/Models/EditorDescriptors/BrandSelectionFactory.cs
@@ -11,19 +11,19 @@ namespace Foundation.Commerce.Models.EditorDescriptors
 {
     public class BrandSelectionFactory : ISelectionFactory
     {
+        private readonly Injected<IContentLoader> _contentLoader;
+        private readonly Injected<ReferenceConverter> _referenceConverter;
+
         public virtual IEnumerable<ISelectItem> GetSelections(ExtendedMetadata metadata)
         {
-            var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
-            var referenceConverter = ServiceLocator.Current.GetInstance<ReferenceConverter>();
-
-            var contentReferences = contentLoader.GetDescendents(referenceConverter.GetRootLink());
+            var contentReferences = _contentLoader.Service.GetDescendents(_referenceConverter.Service.GetRootLink());
 
             if (contentReferences == null || !contentReferences.Any())
             {
                 return new List<SelectItem>();
             }
 
-            var entries = contentLoader.GetItems(contentReferences, CultureInfo.CurrentUICulture);
+            var entries = _contentLoader.Service.GetItems(contentReferences, CultureInfo.CurrentUICulture);
 
             var brands = entries
                 .Where(x => (x as EntryContentBase) != null
--- src/Foundation.Commerce/Models/EditorDescriptors/CatalogSelectionFactory.cs
+++ src/Foundation.Commerce/Models/EditorDescriptors/CatalogSelectionFactory.cs
@@ -10,12 +10,12 @@ namespace Foundation.Commerce.Models.EditorDescriptors
 {
     public class CatalogSelectionFactory : ISelectionFactory
     {
+        private readonly Injected<IContentRepository> _contentRepository;
+        private readonly Injected<ReferenceConverter> _referenceConverter;
+
         public virtual IEnumerable<ISelectItem> GetSelections(ExtendedMetadata metadata)
         {
-            var contentRepository = ServiceLocator.Current.GetInstance<IContentRepository>();
-            var referenceConverter = ServiceLocator.Current.GetInstance<ReferenceConverter>();
-
-            var catalogs = contentRepository.GetChildren<CatalogContentBase>(referenceConverter.GetRootLink());
+            var catalogs = _contentRepository.Service.GetChildren<CatalogContentBase>(_referenceConverter.Service.GetRootLink());
             var items = catalogs.Select(x => new SelectItem { Text = x.Name, Value = x.CatalogId }).ToList();
             items.Insert(0, new SelectItem { Text = "All", Value = 0 });
             return items;
