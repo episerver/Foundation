cb9ae250493c02552b0f3d13637a08fbc2950cce
tanvu-epi <56108258+tanvu-epi@users.noreply.github.com>
2020-01-11 09:16:23 +0700

    #265: not render country list in register form when non-login by default (#273)
    * #265: not render country list in register form when non-login by default
    
    * Update CommerceHeaderViewModelFactory.cs
    
    remove comment lines

--- src/Foundation/Assets/js/features/Address.js
+++ src/Foundation/Assets/js/features/Address.js
@@ -1,14 +1,23 @@
 ﻿class Address {
-    CountryClick() {
-        $('.jsCountrySelectionContainer').change(function () {
+    CountryClick(selector) {
+        if (selector == undefined || selector == "") {
+            selector = ".jsCountrySelectionContainer";
+        }
+
+        $(selector).change(function () {
             var countryCode = $(this).find('input[type=radio]:checked').val();
             var region = $(this).find('input[type=radio]:checked').val();
             var inputName = $(this).closest('form').find('.jsRegionName').val();
             var element = $(this);
             axios.get("/addressbook/GetRegions?countryCode=" + countryCode + "&region=" + region + "&inputName=" + inputName)
                 .then(function (r) {
-                    var region = $(element).closest('form').find('.jsCountryRegionContainer').first();
-                    region.html(r.data);
+                    if ($(element).parents('form').length > 0) {
+                        var region = $(element).closest('form').find('.jsCountryRegionContainer').first();
+                        region.html(r.data);
+                    } else {
+                        var region = $(element).parent().siblings('.jsCountryRegionContainer').first();
+                        region.html(r.data);
+                    }
 
                     feather.replace();
                     var dropdown = new Dropdown(region);
@@ -20,7 +29,41 @@
         })
     }
 
+    LoadAddressInRegisterUser() {
+        var inst = this;
+        $('.jsCountrySelectionRegisterUser').click(function () {
+            var element = this;
+            var data = $(this).find('.jsCountryOptionName').val();
+            if ($(this).find('li').length == 0) {
+                axios.get('/header/getcountryoptions?inputName=' + data)
+                    .then(function (r) {
+                        var html = $(r.data).html();
+                        $(element).html(html);
+                        feather.replace();
+                        var dropdown = new Dropdown(element);
+                        dropdown.Init();
+                        inst.CountryClick(element);
+
+                        $('#login-selector-signup').click(function (e) {
+                            if (!($(e.target).parents('.dropdown').children('.dropdown__selected').length > 0 || $(e.target).hasClass('.dropdown'))) {
+                                $('.dropdown__group').hide();
+                            }
+                        });
+
+                        $(element).find('.dropdown__selected').first().click();
+                    })
+                    .catch(function (e) {
+
+                    })
+                    .finally(function () {
+
+                    })
+            }
+        })
+    }
+
     Init() {
         this.CountryClick();
+        this.LoadAddressInRegisterUser();
     }
 }
