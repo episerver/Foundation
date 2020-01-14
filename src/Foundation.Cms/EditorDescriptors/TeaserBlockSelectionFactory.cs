6c0ebc4c9e2908a85d48cb88331658bdb26945c1
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2020-01-14 17:53:11 +0700

    teaser block enhancement
    #313 add an option to scale image( full or original size), add an option to align elements (image, heading, text) to the left, right or center, add an option to change Heading font size, add more ways to order elements (more options to select rather than image above or below the heading and text), remove unused TeaserBlockWide view

---
+++ src/Foundation.Cms/EditorDescriptors/TeaserBlockSelectionFactory.cs
@@ -0,0 +1,34 @@
+﻿using EPiServer.Shell.ObjectEditing;
+using System.Collections.Generic;
+
+namespace Foundation.Cms.EditorDescriptors
+{
+    class TeaserBlockElementOrderSelectionFactory : ISelectionFactory
+    {
+        public IEnumerable<ISelectItem> GetSelections(ExtendedMetadata metadata)
+        {
+            return new ISelectItem[]
+            {
+                new SelectItem {Text = "Image - Heading - Text", Value = "ImageHeadingText"},
+                new SelectItem {Text = "Image - Text - Heading", Value = "ImageTextHeading"},
+                new SelectItem {Text = "Heading - Image - Text", Value = "HeadingImageText"},
+                new SelectItem {Text = "Heading - Text - Image", Value = "HeadingTextImage"},
+                new SelectItem {Text = "Text - Image - Heading", Value = "TextImageHeading"},
+                new SelectItem {Text = "Text - Heading - Image", Value = "TextHeadingImage"}
+            };
+        }
+    }
+
+    class TeaserBlockElementAlignmentSelectionFactory : ISelectionFactory
+    {
+        public IEnumerable<ISelectItem> GetSelections(ExtendedMetadata metadata)
+        {
+            return new ISelectItem[]
+            {
+                new SelectItem {Text = "Elements align center", Value = "ElementCenter"},
+                new SelectItem {Text = "Elements align left", Value = "ElementLeft"},
+                new SelectItem {Text = "Elements align right", Value = "ElementRight"}
+            };
+        }
+    }
+}
