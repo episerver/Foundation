f75e8f33ba3af1d491ecd172d00d1fbf3c67eb46
Mark Hall <mark.hall@episerver.com>
2020-02-28 11:45:27 -0800

    fix hide header and footer settings and upgrade scenario for new block styles.

--- src/Foundation.Cms/Blocks/FoundationBlockData.cs
+++ src/Foundation.Cms/Blocks/FoundationBlockData.cs
@@ -17,28 +17,43 @@ namespace Foundation.Cms.Blocks
 
         [SelectOne(SelectionFactoryType = typeof(PaddingSelectionFactory))]
         [Display(Name = "Padding", GroupName = CmsTabNames.BlockStyling, Order = 1)]
-        public virtual string Padding { get; set; }
+        public virtual string Padding
+        {
+            get { return this.GetPropertyValue(page => page.Margin) ?? "p-1"; }
+            set { this.SetPropertyValue(page => page.Margin, value); }
+        }
 
         [SelectOne(SelectionFactoryType = typeof(MarginSelectionFactory))]
         [Display(Name = "Margin", GroupName = CmsTabNames.BlockStyling, Order = 2)]
-        public virtual string Margin { get; set; }
+        public virtual string Margin
+        {
+            get { return this.GetPropertyValue(page => page.Margin) ?? "m-0"; }
+            set { this.SetPropertyValue(page => page.Margin, value); }
+        }
 
         [SelectOne(SelectionFactoryType = typeof(BackgroundColorSelectionFactory))]
         [Display(Name = "Background color", GroupName = CmsTabNames.BlockStyling, Order = 3)]
-        public virtual string BackgroundColor { get; set; }
+        public virtual string BackgroundColor
+        {
+            get { return this.GetPropertyValue(page => page.BackgroundColor) ?? "background-color: transparent;"; }
+            set { this.SetPropertyValue(page => page.BackgroundColor, value); }
+        }
 
         [Range(0, 1.0, ErrorMessage = "Opacity only allows value between 0 and 1")]
         [Display(Name = "Block opacity (0 to 1)", GroupName = CmsTabNames.BlockStyling, Order = 4)]
-        public virtual double BlockOpacity { get; set; }
+        public virtual double? BlockOpacity
+        {
+            get { return this.GetPropertyValue(page => page.BlockOpacity) ?? 1; }
+            set { this.SetPropertyValue(page => page.BlockOpacity, value); }
+        }
 
         public override void SetDefaultValues(ContentType contentType)
         {
-            base.SetDefaultValues(contentType);
-
             Padding = "p-1";
             Margin = "m-0";
             BackgroundColor = "background-color: transparent;";
             BlockOpacity = 1;
+            base.SetDefaultValues(contentType);
         }
     }
 }
