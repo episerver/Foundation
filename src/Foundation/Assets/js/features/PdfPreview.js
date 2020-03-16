beeed4e237cafcf63dc0cd41cf24c3dd50e8820d
tanvu-epi <tan.vu@episerver.com>
2020-03-16 16:28:09 +0700

    #431: Adding view for PDF media type

---
+++ src/Foundation/Assets/js/features/PdfPreview.js
@@ -0,0 +1,12 @@
+﻿class PdfPreview {
+    Render() {
+        $('.jsPdfPreview').each(function (i, e) {
+            let url = $(e).attr('mediaUrl');
+            let height = parseInt($(e).attr('height'));
+            if (!height || height <= 0) {
+                height = 800;
+            }
+            PDFObject.embed(url, e, { height: height + "px" });
+        })
+    }
+}
