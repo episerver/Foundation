8b320eb1de046abb4aafa40eb0a52e27a5985feb
Mark Hall <mark.hall@episerver.com>
2020-03-16 16:42:21 -0700

    standard page title improvements

--- src/Foundation.Cms/Pages/StandardPage.cs
+++ src/Foundation.Cms/Pages/StandardPage.cs
@@ -17,12 +17,16 @@ namespace Foundation.Cms.Pages
     {
         [CultureSpecific]
         [SelectOne(SelectionFactoryType = typeof(HeroBlockTextColorSelectionFactory))]
-        [Display(Name = "Page title color", GroupName = SystemTabNames.Content, Order = 203)]
-        public virtual string PageTitleColor { get; set; }
+        [Display(Name = "Title color", GroupName = SystemTabNames.Content, Order = 203)]
+        public virtual string TitleColor
+        {
+            get { return this.GetPropertyValue(page => page.TitleColor) ?? ColorThemes.Light; }
+            set { this.SetPropertyValue(page => page.TitleColor, value); }
+        }
 
         [CultureSpecific]
         [UIHint(UIHint.Image)]
-        [Display(Name = "Background image", GroupName = SystemTabNames.Content, Order = 205)]
+        [Display(Name = "Background image", GroupName = SystemTabNames.Content, Order = 206)]
         public virtual ContentReference BackgroundImage { get; set; }
 
         [CultureSpecific]
@@ -37,11 +41,27 @@ namespace Foundation.Cms.Pages
             Order = 220)]
         public virtual string TopPaddingMode { get; set; }
 
+        [SelectOne(SelectionFactoryType = typeof(BackgroundColorSelectionFactory))]
+        [Display(Name = "Title background color", GroupName = SystemTabNames.Content, Order = 204)]
+        public virtual string TitleBackgroundColor
+        {
+            get { return this.GetPropertyValue(page => page.TitleBackgroundColor) ?? "transparent"; }
+            set { this.SetPropertyValue(page => page.TitleBackgroundColor, value); }
+        }
+
+        [Range(0, 1.0, ErrorMessage = "Opacity only allows value between 0 and 1")]
+        [Display(Name = "Title opacity (0 to 1)", GroupName = SystemTabNames.Content, Order = 205)]
+        public virtual double? TitleOpacity
+        {
+            get { return this.GetPropertyValue(page => page.TitleOpacity) ?? 1; }
+            set { this.SetPropertyValue(page => page.TitleOpacity, value); }
+        }
+
         public override void SetDefaultValues(ContentType contentType)
         {
             base.SetDefaultValues(contentType);
 
-            PageTitleColor = ColorThemes.Light;
+            TitleColor = ColorThemes.Light;
             TopPaddingMode = FoundationStandardPageTopPaddingModeSelectionFactory.FoundationStandardPageTopPaddingModes.Half;
         }
     }
