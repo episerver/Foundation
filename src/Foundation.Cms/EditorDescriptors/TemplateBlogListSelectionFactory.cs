542f8c129597a0c11d709a7345f8cf56b769fd6e
tanvu-epi <56108258+tanvu-epi@users.noreply.github.com>
2020-03-18 02:22:13 +0700

    Feature/422 add new page list view (#428)
    * Update full screen option for blocks
    #421
    - Add an option to make blocks full screen
    - Add an option to make header lager to make it look better with full screen banner (if necessary)
    - Refactor SCSS files
    - Adjust header and footer a bit
    - Change default tab of login user dropdown to Users (for lazy people)
    * Add new page list view

--- src/Foundation.Cms/EditorDescriptors/TemplateBlogListSelectionFactory.cs
+++ src/Foundation.Cms/EditorDescriptors/TemplateBlogListSelectionFactory.cs
@@ -13,6 +13,7 @@ namespace Foundation.Cms.EditorDescriptors
                 new SelectItem { Value = TemplateSelections.ImageLeft, Text = "Image on the left"},
                 new SelectItem { Value = TemplateSelections.ImageTop, Text = "Image on the top"},
                 new SelectItem { Value = TemplateSelections.NoImage, Text = "No image"},
+                new SelectItem { Value = TemplateSelections.Highlight, Text = "Highlight panel"},
             };
         }
     }
@@ -23,5 +24,6 @@ namespace Foundation.Cms.EditorDescriptors
         public const string ImageLeft = "Left";
         public const string ImageTop = "Top";
         public const string NoImage = "NoImage";
+        public const string Highlight = "Highlight";
     }
 }
