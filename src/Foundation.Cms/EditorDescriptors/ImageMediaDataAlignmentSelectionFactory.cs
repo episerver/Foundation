c5dba125aa0d2f65f2f52e1422277b12a39cf49f
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2020-04-03 14:28:34 +0700

    add padding and alignment properties on images

---
+++ src/Foundation.Cms/EditorDescriptors/ImageMediaDataAlignmentSelectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Shell.ObjectEditing;
+using System.Collections.Generic;
+
+namespace Foundation.Cms.EditorDescriptors
+{
+    class ImageMediaDataAlignmentSelectionFactory : ISelectionFactory
+    {
+        public virtual IEnumerable<ISelectItem> GetSelections(ExtendedMetadata metadata)
+        {
+            return new ISelectItem[]
+            {
+                new SelectItem { Text = "Left", Value = "justify-content: flex-start" },
+                new SelectItem { Text = "Center", Value = "justify-content: center" },
+                new SelectItem { Text = "Right", Value = "justify-content: flex-end" },
+            };
+        }
+    }
+}
