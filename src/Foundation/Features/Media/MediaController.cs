16ab9d57dd677e582bc67eb7665dda3b28c9903c
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2020-05-08 02:32:21 +0700

    update some feature for Video file media
    - replace the built-in preview of video by custom preview, so the view
    - of preview on edit mode and the view on published site are the same
    - put Copyright and File size property in use: inject an event to
    - calculate video file size, display Copyright string on right top corner of the video
    - add some properties to control the video
    - video thumbnail has better display to fit the video
    - fix spelling of ColorPickerEditorDescriptor.cs

--- src/Foundation/Features/Media/MediaController.cs
+++ src/Foundation/Features/Media/MediaController.cs
@@ -1,29 +1,54 @@
 ﻿using EPiServer.Core;
+using EPiServer.Editor;
+using EPiServer.Web;
 using EPiServer.Web.Mvc;
+using EPiServer.Web.Routing;
 using Foundation.Cms.Media;
+using Foundation.Cms.ViewModels.Media;
 using System.Web.Mvc;
 
 namespace Foundation.Features.Media
 {
     public class MediaController : PartialContentController<MediaData>
     {
+        private readonly UrlResolver _urlResolver;
+
+        public MediaController(UrlResolver urlResolver)
+        {
+            _urlResolver = urlResolver;
+        }
+
         public override ActionResult Index(MediaData currentContent)
         {
-            if (currentContent is VideoFile)
+            switch (currentContent)
             {
-                return PartialView("~/Features/Media/VideoFile.cshtml", currentContent);
-            }
-            else if (currentContent is ImageMediaData)
-            {
-                return PartialView("~/Features/Media/ImageMedia.cshtml", currentContent);
-            }
-            else if (currentContent is FoundationPdfFile)
-            {
-                return PartialView("~/Features/Media/PdfFile.cshtml", currentContent as FoundationPdfFile);
-            }
-            else
-            {
-                return PartialView("~/Features/Media/Index.cshtml", currentContent.GetType().BaseType.Name);
+                case VideoFile videoFile:
+                    var model = new VideoFileViewModel
+                    {
+                        DisplayControls = videoFile.DisplayControls,
+                        Autoplay = videoFile.Autoplay,
+                        Copyright = videoFile.Copyright
+                    };
+
+                    if (PageEditing.PageIsInEditMode)
+                    {
+                        model.VideoLink = _urlResolver.GetUrl(videoFile.ContentLink, null, new VirtualPathArguments { ContextMode = ContextMode.Default });
+                        model.PreviewImage = ContentReference.IsNullOrEmpty(videoFile.PreviewImage) ? string.Empty :
+                           _urlResolver.GetUrl(videoFile.PreviewImage, null, new VirtualPathArguments { ContextMode = ContextMode.Default });
+                    }
+                    else
+                    {
+                        model.VideoLink = _urlResolver.GetUrl(videoFile.ContentLink);
+                        model.PreviewImage = ContentReference.IsNullOrEmpty(videoFile.PreviewImage) ? string.Empty : _urlResolver.GetUrl(videoFile.PreviewImage);
+                    }
+                   
+                    return PartialView("~/Features/Media/VideoFile.cshtml", model);
+                case ImageMediaData _:
+                    return PartialView("~/Features/Media/ImageMedia.cshtml", currentContent);
+                case FoundationPdfFile _:
+                    return PartialView("~/Features/Media/PdfFile.cshtml", currentContent as FoundationPdfFile);
+                default:
+                    return PartialView("~/Features/Media/Index.cshtml", currentContent.GetType().BaseType.Name);
             }
         }
     }
