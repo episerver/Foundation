2e07b2f1a3de301f76b7996f8df643f2bf7c9be0
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2020-07-20 16:21:48 +0700

    fix hero block responsiveness

--- src/Foundation/Infrastructure/Display/FoundationDisplayModeProvider.cs
+++ src/Foundation/Infrastructure/Display/FoundationDisplayModeProvider.cs
@@ -85,7 +85,7 @@ namespace Foundation.Infrastructure.Display
                     Tag = "displaymode-one-sixth",
                     LargeScreenWidth = 2,
                     MediumScreenWidth = 4,
-                    SmallScreenWidth = 6,
+                    SmallScreenWidth = 12,
                     ExtraSmallScreenWidth = 12
                 }
             };
