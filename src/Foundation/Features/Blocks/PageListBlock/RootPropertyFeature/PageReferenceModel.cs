6ac87d27cc8ecbb617a3c83a88a66657906ed429
Quoc Anh Nguyen <QuocAnh.Nguyen@episerver.com>
2020-08-17 10:46:41 +0700

    Modify RootProperty

---
+++ src/Foundation/Features/Blocks/PageListBlock/RootPropertyFeature/PageReferenceModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using EPiServer.Core;
+using EPiServer.Web;
+
+namespace Foundation.Features.Blocks.PageListBlock.RootPropertyFeature
+{
+    public class PageReferenceModel
+    {
+        public PageReference RootPages { get; set; }
+        // [UIHint(UIHint.Image)]
+        // public ContentReference Image { get; set; }
+    }
+}
