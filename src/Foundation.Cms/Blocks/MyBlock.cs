ab137d9c72dd2b0a61183928594db193b5ef6bdd
Mihail <mikhail.golubev@firstlinesoftware.com>
2020-08-21 12:46:27 +0300

    1 first block type

---
+++ src/Foundation.Cms/Blocks/MyBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace Foundation.Cms.Blocks
+{
+    [ContentType(DisplayName = "My Block",
+        GUID = "8497c00c-1989-4fc9-938b-d0445a00a284",
+        Description = "My first block",
+        GroupName = CmsGroupNames.Content,
+        AvailableInEditMode = true)]
+    [ImageUrl("~/assets/icons/cms/blocks/CMS-icon-block-26.png")]
+    public class MyBlock : FoundationBlockData
+    {
+        [CultureSpecific]
+        [Display(Name = "Header", Order = 10, GroupName = SystemTabNames.Content)]
+        public virtual string Header { get; set; }
+
+        [Display(Name = "Description", Order = 20, GroupName = SystemTabNames.Content)]
+        public virtual XhtmlString Description { get; set; }
+    }
+}
---
+++ src/Foundation/Features/Blocks/MyBlockController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+using Foundation.Cms.Blocks;
+
+namespace Foundation.Features.Blocks
+{
+    public class MyBlockController : BlockController<MyBlock>
+    {
+        public override ActionResult Index(MyBlock currentblock)
+        {
+            return PartialView("~/Features/Blocks/Views/MyBlock.cshtml", currentblock);
+        }
+    }
+}
