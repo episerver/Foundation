7a9d92353350e32068e1347bd4b2c47a21ea48cd
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2020-08-24 10:12:35 +0700

    update culture specific

--- src/Foundation/Features/Settings/SearchSettings.cs
+++ src/Foundation/Features/Settings/SearchSettings.cs
@@ -1,4 +1,5 @@
-﻿using EPiServer.Shell.ObjectEditing;
+﻿using EPiServer.DataAnnotations;
+using EPiServer.Shell.ObjectEditing;
 using Foundation.Cms.Settings;
 using Foundation.Commerce.Models.EditorDescriptors;
 using Foundation.Find.Facets;
@@ -14,32 +15,38 @@ namespace Foundation.Features.Settings
         SettingsName = "Search Settings")]
     public class SearchSettings : SettingsBase, IFacetConfiguration
     {
+        [CultureSpecific]
         [SelectOne(SelectionFactoryType = typeof(SearchOptionSelectionFactory))]
         [Display(Name = "Search option", GroupName = TabNames.SearchSettings, Order = 50)]
         public virtual string SearchOption { get; set; }
 
+        [CultureSpecific]
         [Display(Name = "Show products in search results", GroupName = TabNames.SearchSettings, Order = 100)]
         public virtual bool ShowProductSearchResults { get; set; }
 
+        [CultureSpecific]
         [Display(Name = "Show contents in search results", GroupName = TabNames.SearchSettings, Order = 150)]
         public virtual bool ShowContentSearchResults { get; set; }
 
+        [CultureSpecific]
         [Display(Name = "Show PDFs in search results", GroupName = TabNames.SearchSettings, Order = 175)]
         public virtual bool ShowPdfSearchResults { get; set; }
 
+        [CultureSpecific]
         [Display(Name = "Include images in contents search results", GroupName = TabNames.SearchSettings, Order = 200)]
         public virtual bool IncludeImagesInContentsSearchResults { get; set; }
 
+        [CultureSpecific]
         [SelectOne(SelectionFactoryType = typeof(CatalogSelectionFactory))]
         [Display(Name = "Search catalog", GroupName = TabNames.SearchSettings, Order = 250,
             Description = "The catalogs that will be returned by search.")]
         public virtual int SearchCatalog { get; set; }
 
-        [Display(
-          Name = "Search Filters Configuration",
-          Description = "Manage filters to be displayed on Search",
-          GroupName = TabNames.SearchSettings,
-          Order = 300)]
+        [CultureSpecific]
+        [Display(Name = "Search Filters Configuration",
+            Description = "Manage filters to be displayed on Search",
+            GroupName = TabNames.SearchSettings,
+            Order = 300)]
         [EditorDescriptor(EditorDescriptorType = typeof(IgnoreCollectionEditorDescriptor<FacetFilterConfigurationItem>))]
         public virtual IList<FacetFilterConfigurationItem> SearchFiltersConfiguration { get; set; }
     }
