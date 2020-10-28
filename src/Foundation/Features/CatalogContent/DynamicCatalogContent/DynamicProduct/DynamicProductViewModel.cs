4fef4b034f1607a90972155e30735d37ff9007ce
tanvu-epi <tan.vu@episerver.com>
2020-10-28 10:32:03 +0700

    #652: Dynamic Product with Variant linl to Variants

---
+++ src/Foundation/Features/CatalogContent/DynamicCatalogContent/DynamicProduct/DynamicProductViewModel.cs
@@ -0,0 +1,118 @@
+﻿using EPiServer;
+using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Marketing.Testing.Core.DataClass;
+using EPiServer.Personalization.Commerce.Tracking;
+using EPiServer.ServiceLocation;
+using Foundation.Commerce.Markets;
+using Foundation.Features.CatalogContent.DynamicCatalogContent.DynamicVariation;
+using Foundation.Features.CatalogContent.Product;
+using Foundation.Features.CatalogContent.Services;
+using Foundation.Features.CatalogContent.Variation;
+using Foundation.Social.ViewModels;
+using Mediachase.Commerce;
+using Mediachase.Commerce.Catalog;
+using Mediachase.Commerce.Pricing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Foundation.Features.CatalogContent.DynamicCatalogContent.DynamicProduct
+{
+    public class DynamicProductViewModel : ProductViewModelBase<DynamicProduct, DynamicVariant>, IEntryViewModelBase
+    {
+        public DynamicProductViewModel()
+        {
+            GroupVariants = new List<VariantGroupModel>();
+        }
+
+        public DynamicProductViewModel(DynamicProduct currentContent) : base(currentContent)
+        {
+            GroupVariants = new List<VariantGroupModel>();
+        }
+
+        public ReviewsViewModel Reviews { get; set; }
+        public IEnumerable<Recommendation> AlternativeProducts { get; set; }
+        public IEnumerable<Recommendation> CrossSellProducts { get; set; }
+        public List<VariantGroupModel> GroupVariants { get; set; }
+    }
+
+    public static class DynamicProductViewModelExtensions
+    {
+        private static readonly Injected<IPromotionService> _promotionService;
+        private static readonly Injected<IPriceService> _priceService;
+        private static readonly Injected<IContentLoader> _contentLoader;
+        private static readonly Injected<ICurrentMarket> _currentMarket;
+        private static readonly Injected<ICurrencyService> _currencyservice;
+
+        public static void GenerateVariantGroup(this DynamicProductViewModel model)
+        {
+            var variantGroups = model.Variant.VariantsOptions.GroupBy(x => x.GroupName);
+            var market = _currentMarket.Service.GetCurrentMarket();
+            var currency = _currencyservice.Service.GetCurrentCurrency();
+
+            if (variantGroups != null)
+            {
+                var groups = new List<VariantGroupModel>();
+                foreach (var group in variantGroups)
+                {
+                    var groupModel = new VariantGroupModel();
+                    groupModel.GroupName = group.Key;
+
+                    foreach (var v in group)
+                    {
+                        var variantModel = new VariantOptionModel();
+                        var defaultPrice = GetDefaultPrice(model.Variant.Code, market, currency);
+                        variantModel.Variant = _contentLoader.Service.Get<VariationContent>(v.Variant) as GenericVariant;
+                        variantModel.DefaultPrice = defaultPrice?.UnitPrice ?? new Money(0, currency);
+                        variantModel.DiscountedPrice = GetDiscountPrice(defaultPrice, market, currency);
+
+                        groupModel.Variants.Add(variantModel);
+                    }
+
+                    groups.Add(groupModel);
+                }
+
+
+                model.GroupVariants = groups;
+            }
+        }
+
+        private static IPriceValue GetDefaultPrice(string entryCode, IMarket market, Currency currency)
+        {
+            return _priceService.Service.GetDefaultPrice(
+                market.MarketId,
+                DateTime.Now,
+                new CatalogKey(entryCode),
+                currency);
+        }
+
+        private static Money? GetDiscountPrice(IPriceValue defaultPrice, IMarket market, Currency currency)
+        {
+            if (defaultPrice == null)
+            {
+                return null;
+            }
+
+            return _promotionService.Service.GetDiscountPrice(defaultPrice.CatalogKey, market.MarketId, currency).UnitPrice;
+        }
+
+    }
+
+
+    public class VariantGroupModel
+    {
+        public VariantGroupModel()
+        {
+            Variants = new List<VariantOptionModel>();
+        }
+        public string GroupName { get; set; }
+        public List<VariantOptionModel> Variants { get; set; }
+    }
+
+    public class VariantOptionModel
+    {
+        public GenericVariant Variant { get; set; }
+        public Money DefaultPrice { get; set; }
+        public Money? DiscountedPrice { get; set; }
+    }
+}
---
+++ src/Foundation/Features/CatalogContent/DynamicCatalogContent/DynamicVariation/DynamicVariant.cs
@@ -0,0 +1,65 @@
+﻿using EPiServer.Cms.Shell.UI.ObjectEditing.EditorDescriptors;
+using EPiServer.Commerce;
+using EPiServer.Commerce.Catalog.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAnnotations;
+using EPiServer.PlugIn;
+using EPiServer.Shell.ObjectEditing;
+using Foundation.Features.CatalogContent.Variation;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Foundation.Features.CatalogContent.DynamicCatalogContent.DynamicVariation
+{
+    [CatalogContentType(DisplayName = "Dynamic Variant", GUID = "11c2960f-79d6-4876-8d8a-6b7bc8cfe869", Description = "Dynamic variant supports multiple variation types")]
+    [ImageUrl("~/assets/icons/cms/pages/cms-icon-page-23.png")]
+    public class DynamicVariant : GenericVariant
+    {
+        [BackingType(typeof(VariantGroupPropertyList))]
+        [Display(GroupName = "Variants Options", Order = 400)]
+        [EditorDescriptor(EditorDescriptorType = typeof(CollectionEditorDescriptor<VariantGroup>))]
+        public virtual IList<VariantGroup> VariantsOptions { get; set; }
+    }
+
+
+    public class VariantGroup
+    {
+        [UIHint(UIHint.ProductVariation)]
+        [Display(Name = "Variation")]
+        public virtual ContentReference Variant { get; set; }
+
+        [Display(Name = "Group name")]
+        public virtual string GroupName { get; set; }
+    }
+
+    [PropertyDefinitionTypePlugIn]
+    public class VariantGroupPropertyList : PropertyList<VariantGroup>
+    {
+        protected override VariantGroup ParseItem(string value)
+        {
+            return JsonConvert.DeserializeObject<VariantGroup>(value);
+        }
+        
+        [Obsolete("ParseToObject is no longer required to be implemented. The same functionality can be achieved by creating a new instance and calling the ParseToSelf method.")]
+        public override PropertyData ParseToObject(string value)
+        {
+            var prop = new VariantGroupPropertyList();
+            prop.ParseToSelf(value);
+            return prop;
+        }
+
+        public override string ToString()
+        {
+            if (IsNull)
+            {
+                return string.Empty;
+            }
+            return string.Join(StringRepresentationSeparator.ToString(),
+                List.Select(x => x.ToString()));
+            //this needs to handle representing the object as a string
+        }
+    }
+}
