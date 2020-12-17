43ccda31f906867b5cac90936d158f40be3d7ede
Ngoc Nguyen <56108286+ngocnguyen-epi@users.noreply.github.com>
2020-12-17 15:13:57 +0700

    consistency update (#675)

--- src/Foundation/Features/Blocks/TeaserBlock/TeaserBlock.cs
+++ src/Foundation/Features/Blocks/TeaserBlock/TeaserBlock.cs
@@ -32,6 +32,14 @@ namespace Foundation.Features.Blocks.TeaserBlock
         [Display(Name = "Heading style", GroupName = SystemTabNames.Content, Order = 12)]
         public virtual string HeadingStyle { get; set; }
 
+        [ClientEditor(ClientEditingClass = "foundation/editors/ColorPicker")]
+        [Display(Name = "Heading color", GroupName = SystemTabNames.Content, Order = 13)]
+        public virtual string HeadingColor
+        {
+            get { return this.GetPropertyValue(page => page.HeadingColor) ?? "#000000ff"; }
+            set { this.SetPropertyValue(page => page.HeadingColor, value); }
+        }
+
         [CultureSpecific]
         [Display(GroupName = SystemTabNames.Content, Order = 20)]
         public virtual string Description { get; set; }
@@ -40,7 +48,6 @@ namespace Foundation.Features.Blocks.TeaserBlock
         [Display(GroupName = SystemTabNames.Content, Order = 30)]
         public virtual XhtmlString Text { get; set; }
 
-        [Required]
         [CultureSpecific]
         [UIHint(UIHint.Image)]
         [Display(GroupName = SystemTabNames.Content, Order = 40)]
@@ -50,32 +57,41 @@ namespace Foundation.Features.Blocks.TeaserBlock
         [Display(Name = "Image size (%)", GroupName = SystemTabNames.Content, Order = 41)]
         public virtual int ImageSize { get; set; }
 
-        [SelectOne(SelectionFactoryType = typeof(TeaserBlockTextColorSelectionFactory))]
-        [Display(Name = "Text color", GroupName = SystemTabNames.Content, Order = 51)]
-        public virtual string TextColor { get; set; }
+        [CultureSpecific]
+        [UIHint(UIHint.Image)]
+        [Display(Name = "Second Image", GroupName = SystemTabNames.Content, Order = 45)]
+        public virtual ContentReference SecondImage { get; set; }
 
-        [SelectOne(SelectionFactoryType = typeof(TeaserBlockElementOrderSelectionFactory))]
-        [Display(Name = "Elements order", GroupName = SystemTabNames.Content, Order = 80)]
-        public virtual string ElementsOrder { get; set; }
-
-        [SelectOne(SelectionFactoryType = typeof(TeaserBlockElementAlignmentSelectionFactory))]
-        [Display(Name = "Elements alignment (except Text)", GroupName = SystemTabNames.Content, Order = 81)]
-        public virtual string ElementsAlignment { get; set; }
+        [Range(1, 100, ErrorMessage = "Set image width from 1 to 100")]
+        [Display(Name = "Image size (%)", GroupName = SystemTabNames.Content, Order = 46)]
+        public virtual int SecondImageSize { get; set; }
 
         [Display(GroupName = SystemTabNames.Content, Order = 90)]
         public virtual PageReference Link { get; set; }
+
+        [ClientEditor(ClientEditingClass = "foundation/editors/ColorPicker")]
+        [Display(Name = "Text color", GroupName = SystemTabNames.Content, Order = 50)]
+        public virtual string TextColor
+        {
+            get { return this.GetPropertyValue(page => page.TextColor) ?? "#000000ff"; }
+            set { this.SetPropertyValue(page => page.TextColor, value); }
+        }
+
+        [SelectOne(SelectionFactoryType = typeof(TeaserBlockHeightStyleSelectionFactory))]
+        [Display(Name = "Height", GroupName = TabNames.BlockStyling, Order = 100)]
+        public virtual string Height { get; set; }
 
         public override void SetDefaultValues(ContentType contentType)
         {
             base.SetDefaultValues(contentType);
 
             HeadingSize = 28;
-            HeadingStyle = "text-decoration: none";
+            HeadingStyle = "none";
+            HeadingColor = "#000000ff";
             ImageSize = 100;
-            ElementsOrder = "ImageHeadingDescription";
-            ElementsAlignment = "text-align: center";
+            SecondImageSize = 100;
             BackgroundColor = "transparent";
-            TextColor = "color: black";
+            TextColor = "#000000ff";
         }
 
         public void SetItem(ItemModel itemModel)
