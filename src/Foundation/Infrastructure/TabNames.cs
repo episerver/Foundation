6ba5ca151bfa324655c1e4eec9266825ece58913
Quoc Anh Nguyen <QuocAnh.Nguyen@episerver.com>
2021-04-01 14:40:31 +0700

    Add tab names

--- src/Foundation/Infrastructure/TabNames.cs
+++ src/Foundation/Infrastructure/TabNames.cs
@@ -79,17 +79,23 @@ namespace Foundation.Infrastructure
         [RequiredAccess(AccessLevel.Edit)]
         public const string Scripts = "Scripts";
 
+        [Display(Name = "Text", Order = 281)]
+        public const string Text = "Text";
+
         [Display(Name = "Background", Order = 283)]
         public const string Background = "Background";
 
         [Display(Name = "Border", Order = 284)]
         public const string Border = "Border";
 
-        [Display(Name = "Block styling", Order = 285)]
-        public const string BlockStyling = "BlockStyling";
-
-        [Display(Name = "Colors", Order = 289)]
+        [Display(Name = "Colors", Order = 285)]
         public const string Colors = "Colors";
+
+        [Display(Name = "Image", Order = 286)]
+        public const string Image = "Image";
+
+        [Display(Name = "Block styling", Order = 287)]
+        public const string BlockStyling = "BlockStyling";
 
         [Display(Name = "Settings", Order = 290)]
         public const string Settings = SystemTabNames.Settings;
