2d37d2d2602b413a26709e0bef586f6ac0dbabcb
Ngoc Nguyen <56108286+ngocnguyen-epi@users.noreply.github.com>
2021-04-05 09:33:47 +0700

    fix the page shake on edit mode (#703)

--- src/Foundation/Features/Blocks/PageListBlock/PageListBlock.cs
+++ src/Foundation/Features/Blocks/PageListBlock/PageListBlock.cs
@@ -65,7 +65,7 @@ namespace Foundation.Features.Blocks.PageListBlock
         [SelectOne(SelectionFactoryType = typeof(TemplateListSelectionFactory))]
         public virtual string Template { get; set; }
 
-        [Display(Name = "Preview option (only for 'Image on the top' and 'Image on the left' templates)", GroupName = SystemTabNames.Content, Order = 110)]
+        [Display(Name = "Preview option (not available in the Grid, Insight templates)", GroupName = SystemTabNames.Content, Order = 110)]
         [SelectOne(SelectionFactoryType = typeof(PreviewOptionSelectionFactory))]
         public virtual string PreviewOption { get; set; }
 
@@ -96,15 +96,15 @@ namespace Foundation.Features.Blocks.PageListBlock
     {
         public IEnumerable<ValidationError> Validate(PageListBlock block)
         {
-            if (block.Template == TemplateSelections.Card || block.Template == TemplateSelections.Insight)
+            if (block.Template == TemplateSelections.Insight)
             {
-                if (block.Count % 6 != 0)
+                if (block.Count % 3 != 0)
                 {
                     return new ValidationError[]
                     {
                         new ValidationError()
                         {
-                             ErrorMessage = "The property Count must be divisible by 6 (with Template is Card or Insight)",
+                             ErrorMessage = "The property Number of results must be divisible by 3 if Template is Insight",
                              PropertyName = block.GetPropertyName(p => p.Count),
                              Severity = ValidationErrorSeverity.Error,
                              ValidationType = ValidationErrorType.StorageValidation
