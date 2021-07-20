7bb9b8e2b179782d9c88a9fbc578b1567fdc1313
Ngoc Nguyen <56108286+ngocnguyen-epi@users.noreply.github.com>
2021-07-20 15:24:39 +0700

    Add OPE for Reset Password Mail Page (#738)

---
+++ src/Foundation/Features/MyAccount/ResetPassword/ResetPasswordMailController.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Web.Mvc;
+using System.Web.Mvc;
+
+namespace Foundation.Features.MyAccount.ResetPassword
+{
+    public class ResetPasswordMailController : PageController<ResetPasswordMailPage>
+    {
+        public ActionResult Index(ResetPasswordMailPage currentPage)
+        {
+            var model = new ResetPasswordMailViewModel(currentPage);
+            return View("ResetPasswordMail", model);
+        }
+    }
+}
