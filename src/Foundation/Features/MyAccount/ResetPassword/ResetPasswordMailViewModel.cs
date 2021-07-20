bdcaf62bb487d1372c5204f1c1c8d3f277ee9e66
Ngoc Nguyen <ngoc.nguyen@episerver.com>
2021-07-20 15:05:28 +0700

    Add OPE for Reset Password Mail Page

---
+++ src/Foundation/Features/MyAccount/ResetPassword/ResetPasswordMailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Foundation.Features.Shared;
+
+namespace Foundation.Features.MyAccount.ResetPassword
+{
+    public class ResetPasswordMailViewModel : ContentViewModel<ResetPasswordMailPage>
+    {
+        public ResetPasswordMailViewModel()
+        {
+        }
+
+        public ResetPasswordMailViewModel(ResetPasswordMailPage resetPasswordMailPage) : base(resetPasswordMailPage)
+        {
+        }
+    }
+}
