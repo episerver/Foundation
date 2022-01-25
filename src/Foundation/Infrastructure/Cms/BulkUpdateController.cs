002f5b9f0ff4713eb89a4764f62438d79286524b
Mark Hall <mark.hall@episerver.com>
2022-01-25 11:23:50 -0800

    Fix BulkUpdateController and view

--- src/Foundation/Infrastructure/Cms/BulkUpdateController.cs
+++ src/Foundation/Infrastructure/Cms/BulkUpdateController.cs
@@ -1,4 +1,7 @@
 ﻿using EPiServer;
+using EPiServer.ContentApi.Core.Configuration;
+using EPiServer.ContentApi.Core.Serialization;
+using EPiServer.ContentApi.Core.Serialization.Models;
 using EPiServer.Core;
 using EPiServer.Data.Entity;
 using EPiServer.DataAbstraction;
@@ -6,17 +9,19 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Foundation.Infrastructure.Cms
 {
     public class BulkUpdateController : Controller
     {
-        private readonly IContentModelMapper _contentModelMapper;
+        private readonly IContentConverterProvider _contentConverterProvider;
         private readonly IContentTypeRepository _contentTypeRepository;
         private readonly IContentRepository _contentRepository;
         private readonly ILanguageBranchRepository _languageBranchRepository;
         private readonly IContentLoader _contentLoader;
+        private readonly ContentApiOptions _contentApiOptions;
         private const int InformationBitCount = 30;
         public const string CatalogProviderKey = "CatalogContent";
         private readonly Dictionary<string, Type> _contentTypes = new Dictionary<string, Type>()
@@ -30,28 +35,31 @@ namespace Foundation.Infrastructure.Cms
             { "Discount",  Type.GetType("EPiServer.Commerce.Marketing.PromotionData, EPiServer.Business.Commerce", false) }
         };
 
-        public BulkUpdateController(IContentModelMapper contentModelMapper,
+        public BulkUpdateController(IContentConverterProvider contentConverterProvider,
             IContentTypeRepository contentTypeRepository,
             IContentRepository contentRepository,
             ILanguageBranchRepository languageBranchRepository,
-            IContentLoader contentLoader)
+            IContentLoader contentLoader, 
+            ContentApiOptions contentApiOptions)
         {
-            _contentModelMapper = contentModelMapper;
+            _contentConverterProvider = contentConverterProvider;
             _contentTypeRepository = contentTypeRepository;
             _contentRepository = contentRepository;
             _languageBranchRepository = languageBranchRepository;
             _contentLoader = contentLoader;
+            _contentApiOptions = contentApiOptions;
         }
 
         [HttpGet]
-        //[MenuItem("/global/foundation/bulkupdate", Text = "Bulk Update", TextResourceKey = "Bulk Update", SortIndex = 15)]
+        [Route("episerver/foundation/bulkUpdate", Name = "bulkUpdate")]
         public ActionResult Index()
         {
-            return View();
+            return View("/Infrastructure/Cms/Views/BulkUpdate/Index.cshtml");
         }
 
         [HttpGet]
-        public ActionResult GetContentTypes(string type)
+        [Route("episerver/foundation/bulkUpdate/getContentTypes/{type}", Name = "bulkUpdate_getContentTypes")]
+        public ActionResult GetContentTypes([FromRoute] string type)
         {
             var contentTypes = _contentTypeRepository.List().Where(o => o.Name != "SysRoot" && o.Name != "SysRecycleBin" && IsValidType(type, o.ModelType))
                 .Select(o => new
@@ -70,7 +78,8 @@ namespace Foundation.Infrastructure.Cms
         }
 
         [HttpGet]
-        public ActionResult GetProperties(int id)
+        [Route("episerver/foundation/bulkUpdate/getProperties/{id:int}", Name = "bulkUpdate_getProperties")]
+        public ActionResult GetProperties([FromRoute] int id)
         {
             var contentType = _contentTypeRepository.Load(id);
             var properties = contentType.PropertyDefinitions
@@ -93,6 +102,7 @@ namespace Foundation.Infrastructure.Cms
         }
 
         [HttpGet]
+        [Route("episerver/foundation/bulkUpdate/getLanguages", Name = "bulkUpdate_getLanguages")]
         public ActionResult GetLanguages()
         {
             var languages = _languageBranchRepository.ListEnabled().Select(o => new
@@ -109,7 +119,8 @@ namespace Foundation.Infrastructure.Cms
         }
 
         [HttpGet]
-        public ActionResult Get(int contentTypeId, string language, string properties, string keyword = "")
+        [Route("episerver/foundation/bulkUpdate/getContent", Name = "bulkUpdate_getContent")]
+        public ActionResult Get([FromQuery] int contentTypeId, [FromQuery] string language, [FromQuery] string properties, [FromQuery] string keyword = "")
         {
             var contentType = _contentTypeRepository.Load(contentTypeId);
             var catalogContent = Type.GetType("EPiServer.Commerce.Catalog.ContentTypes.CatalogContentBase, EPiServer.Business.Commerce", false)?.IsAssignableFrom(contentType.ModelType) ?? false;
@@ -120,7 +131,15 @@ namespace Foundation.Infrastructure.Cms
             {
                 contents = contents.Where(o => o.Name.IndexOf(keyword, StringComparison.CurrentCultureIgnoreCase) >= 0);
             }
-            var models = contents.Select(o => _contentModelMapper.TransformContent(o, false, properties));
+            var models = contents.Select(o => _contentConverterProvider.Resolve(o).Convert(o, new ConverterContext
+            (
+                _contentApiOptions,
+                "",
+                "",
+                false,
+                CultureInfo.GetCultureInfo(language)
+            )));
+
             return new ContentResult
             {
                 Content = JsonConvert.SerializeObject(models),
@@ -129,7 +148,8 @@ namespace Foundation.Infrastructure.Cms
         }
 
         [HttpPost]
-        public ActionResult UpdateContent(UpdateContentModel updateContentModel)
+        [Route("episerver/foundation/bulkUpdate/updateContent", Name = "bulkUpdate_updateContent")]
+        public ActionResult UpdateContent([FromBody] UpdateContentModel updateContentModel)
         {
             var props = updateContentModel.Properties.Split(',');
             var message = "";
