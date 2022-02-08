da29fd011d61c41623151edf3f7e29de35150f39
Hiep To <hiep.todinh@optimizely.com>
2022-02-08 18:41:39 +0100

    Fix checkout TempData exception - Microsoft.AspNetCore.Mvc.ViewFeatures.Infrastructure.DefaultTempDataSerializer' cannot serialize an object of type 'System.Collections.Generic.List

--- src/Foundation/Infrastructure/Helpers/Extensions.cs
+++ src/Foundation/Infrastructure/Helpers/Extensions.cs
@@ -7,6 +7,8 @@ using Foundation.Features.Login;
 using Foundation.Features.Settings;
 using Foundation.Infrastructure.Cms.Settings;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
 using System;
 
 namespace Foundation.Infrastructure.Helpers
@@ -31,6 +33,18 @@ namespace Foundation.Infrastructure.Helpers
             model.Title = title;
             model.LoginViewModel.ReturnUrl = returnUrl;
             return model;
+        }
+
+        public static void Set<T>(this ITempDataDictionary tempData, string key, T value) where T : class
+        {
+            tempData[key] = JsonConvert.SerializeObject(value);
+        }
+
+        public static T Get<T>(this ITempDataDictionary tempData, string key) where T : class
+        {
+            object o;
+            tempData.TryGetValue(key, out o);
+            return o == null ? null : JsonConvert.DeserializeObject<T>((string)o);
         }
     }
 }
