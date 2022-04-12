ab00ebe92e89ccb95b15fef6372cf234751e4e26
Javed Shaikh <javed.shaikh@gmail.com>
2022-04-12 09:41:32 -0700

    Checking in minor update

--- src/Foundation/Features/Preview/PreviewController.cs
+++ src/Foundation/Features/Preview/PreviewController.cs
@@ -47,8 +47,8 @@ namespace Foundation.Features.Preview
 
             if (!supportedDisplayOptions.Any(x => x.Supported))
             {
-                var itemType = currentContent.GetType();
-                if (itemType.Name.Contains("MenuItemBlock")) // handle exception while previewing menu item
+                //var itemType = currentContent.GetType();
+                if (currentContent is MenuItemBlock) // itemType.Name.Contains("MenuItemBlock") handle exception while previewing menu item
                     return View("~/Features/Preview/Index.cshtml", model);
                 else
                 {
