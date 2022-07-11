e8bd2201d419d484146c19995b1c47cbaa97997d
Daniel Isaacs <daniel.isaacs@episerver.com>
2022-07-11 23:11:53 +0000

    Update col-xs-12 to col-12 due to bootstrap changes

--- src/Foundation/Infrastructure/Display/DisplayExtensions.cs
+++ src/Foundation/Infrastructure/Display/DisplayExtensions.cs
@@ -11,12 +11,12 @@ namespace Foundation.Infrastructure.Display
             {
                 displayOption.Add("screen", "/displayoptions/screen", "screen-width-block", "", "epi-icon__layout--screen");
                 displayOption.Add("full", "/displayoptions/full", "col-12", "", "epi-icon__layout--full");
-                displayOption.Add("threequarter", "/displayoptions/threequarter", "col-lg-9 col-md-6 col-sm-12 col-xs-12 displaymode-three-quarters", "", "epi-icon__layout--three-quarters");
-                displayOption.Add("half", "/displayoptions/half", "col-lg-6 col-md-6 col-sm-12 col-xs-12", "", "epi-icon__layout--half");
-                displayOption.Add("wide", "/displayoptions/wide", "col-lg-8 col-md-6 col-sm-12 col-xs-12", "", "epi-icon__layout--two-thirds");
-                displayOption.Add("narrow", "/displayoptions/narrow", "col-lg-4 col-md-6 col-sm-12 col-xs-12", "", "epi-icon__layout--one-third");
-                displayOption.Add("one-quarter", "/displayoptions/one-quarter", "col-lg-3 col-md-6 col-sm-12 col-xs-12", "", "epi-icon__layout--one-quarter");
-                displayOption.Add("one-sixth", "/displayoptions/one-sixth", "col-lg-2 col-md-4 col-sm-12 col-xs-12 displaymode-one-sixth", "", "epi-icon__layout--one-sixth");
+                displayOption.Add("threequarter", "/displayoptions/threequarter", "col-lg-9 col-md-6 col-sm-12 col-12 displaymode-three-quarters", "", "epi-icon__layout--three-quarters");
+                displayOption.Add("wide", "/displayoptions/wide", "col-lg-8 col-md-6 col-sm-12 col-12", "", "epi-icon__layout--two-thirds");
+                displayOption.Add("half", "/displayoptions/half", "col-lg-6 col-md-6 col-sm-12 col-12", "", "epi-icon__layout--half");
+                displayOption.Add("narrow", "/displayoptions/narrow", "col-lg-4 col-md-6 col-sm-12 col-12", "", "epi-icon__layout--one-third");
+                displayOption.Add("one-quarter", "/displayoptions/one-quarter", "col-lg-3 col-md-6 col-sm-12 col-12", "", "epi-icon__layout--one-quarter");
+                displayOption.Add("one-sixth", "/displayoptions/one-sixth", "col-lg-2 col-md-4 col-sm-12 col-12 displaymode-one-sixth", "", "epi-icon__layout--one-sixth");
             });
 
             services.AddDisplayResolutions();
