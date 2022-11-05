6a1a3cb22a9072c65b7cfee375ef0bc2ea009216
Daniel Isaacs <daniel.isaacs@episerver.com>
2022-11-05 01:14:09 +0000

    Add Modal Block type (#890)

---
+++ src/Foundation/Features/Blocks/ModalBlock/ModalBlockComponent.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Foundation.Features.Blocks.ModalBlock
+{
+    public class ModalBlockComponent : AsyncBlockComponent<ModalBlock>
+    {
+        protected override async Task<IViewComponentResult> InvokeComponentAsync(ModalBlock currentBlock)
+        {
+            return await Task.FromResult(View("~/Features/Blocks/ModalBlock/ModalBlock.cshtml", currentBlock));
+        }
+    }
+}
