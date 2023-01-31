df591b32b20cbe75d44935c78a5d80fd4eb97a76
Daniel Isaacs <daniel.isaacs@episerver.com>
2023-01-30 20:22:25 -0800

    Update packages and configure new TinyMCE version (#904)

--- src/Foundation/Infrastructure/Cms/Extensions/ServiceConfigurationContextExtensions.cs
+++ src/Foundation/Infrastructure/Cms/Extensions/ServiceConfigurationContextExtensions.cs
@@ -10,18 +10,21 @@ namespace Foundation.Infrastructure.Cms.Extensions
             services.Configure<TinyMceConfiguration>(config =>
             {
                 config.Default()
-                    .AddPlugin("media wordcount anchor code textcolor colorpicker")
-                    .Toolbar("formatselect | epi-personalized-content epi-link anchor numlist bullist indent outdent bold italic underline alignleft aligncenter alignright | image epi-image-editor media code | epi-dnd-processor | removeformat | fullscreen | forecolor backcolor | icons")
+                    .AddPlugin("media wordcount anchor code")
+                    .Toolbar("blocks | epi-personalized-content epi-link anchor numlist bullist indent outdent bold italic underline | code",
+                        "alignleft aligncenter alignright alignjustify | image epi-image-editor media | epi-dnd-processor | forecolor backcolor | removeformat | searchreplace fullscreen")
                     .AddSetting("image_caption", true)
-                    .AddSetting("image_advtab", true);
+                    .AddSetting("image_advtab", true)
+                    .AddSetting("resize", "both")
+                    .AddSetting("height", 400);
 
                 config.Default()
                     .AddEpiserverSupport()
-                    .AddExternalPlugin("icons", "/ClientResources/Scripts/fontawesomeicons.js")
-                    .AddSetting("extended_valid_elements", "i[class], span")
-                    .ContentCss(new[] { "/ClientResources/Styles/fontawesome.min.css",
-                        "https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i",
-                        "/ClientResources/Styles/TinyMCE.css" });
+                    //.AddExternalPlugin("icons", "/ClientResources/Scripts/fontawesomeicons.js")
+                    .AddSetting("extended_valid_elements", "i[class], span");
+                    //.ContentCss(new[] { "/ClientResources/Styles/fontawesome.min.css",
+                    //    "https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i",
+                    //    "/ClientResources/Styles/TinyMCE.css" });
             });
         }
     }
