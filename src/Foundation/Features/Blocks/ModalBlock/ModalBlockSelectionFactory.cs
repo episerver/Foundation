564d0e8734d5c8ee2506e89fb2a6a796b51ab68c
Daniel Isaacs <daniel.isaacs@episerver.com>
2023-02-19 12:43:54 -0800

    Update Modal Block (#913)

---
+++ src/Foundation/Features/Blocks/ModalBlock/ModalBlockSelectionFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Foundation.Features.Blocks.ModalBlock
+{
+    public class ModalSizeSelectionFactory : ISelectionFactory
+    {
+        public virtual IEnumerable<ISelectItem> GetSelections(ExtendedMetadata metadata)
+        {
+            return new ISelectItem[]
+            {
+                new SelectItem { Text = "Default (500px)", Value = "" },
+                new SelectItem { Text = "Small (300px)", Value = "modal-sm" },
+                new SelectItem { Text = "Large (800px)", Value = "modal-lg" },
+                new SelectItem { Text = "Extra Large (1140px)", Value = "modal-xl" },
+            };
+        }
+    }
+}
