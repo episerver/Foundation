1966782394510045e3b9ae8f10f95816e37fa925
Daniel Isaacs <daniel.isaacs@episerver.com>
2023-07-06 22:05:09 +0000

    Update how media types are handled

--- src/Foundation/Features/Media/StandardFile.cs
+++ src/Foundation/Features/Media/StandardFile.cs
@@ -6,6 +6,20 @@ namespace Foundation.Features.Media
     [MediaDescriptor(ExtensionString = "txt,doc,docx,xls,xlsx,ppt,pptx")]
     public class StandardFile : MediaData
     {
+        [CultureSpecific]
+        [Display(GroupName = SystemTabNames.Content, Order = 10)]
+        public virtual string Title { get; set; }
+
+        [CultureSpecific]
+        [Display(GroupName = SystemTabNames.Content, Order = 20)]
+        public virtual string Description { get; set; }
+
+        [Display(Name = "Show Description?", GroupName = SystemTabNames.Content, Order = 30)]
+        public virtual bool ShowDescription { get; set; }
+
+        [Display(Name = "Show Icon?", GroupName = SystemTabNames.Content, Order = 30)]
+        public virtual bool ShowIcon { get; set; }
+
         [Editable(false)]
         public virtual string FileSize { get; set; }
     }
