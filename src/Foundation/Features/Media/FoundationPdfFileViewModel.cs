79d644a3674770891a56552e87ae6fe4377c7fea
Daniel Isaacs <daniel.isaacs@episerver.com>
2023-07-12 13:37:02 +0000

    Update how media types are handled (#936)

--- src/Foundation/Features/Media/FoundationPdfFileViewModel.cs
+++ src/Foundation/Features/Media/FoundationPdfFileViewModel.cs
@@ -2,8 +2,14 @@
 {
     public class FoundationPdfFileViewModel
     {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool ShowDescription { get; set; }
+        public bool ShowIcon { get; set; }
         public int Id { get; set; }
         public int Height { get; set; }
         public string PdfLink { get; set; }
+        public bool DisplayAsPreview { get; set; }
     }
 }
--- src/Foundation/Features/Media/ImageMediaDataViewModel.cs
+++ src/Foundation/Features/Media/ImageMediaDataViewModel.cs
@@ -6,6 +6,8 @@
         public string ImageLink { get; set; }
         public string LinkToContent { get; set; }
         public string Description { get; set; }
+        public string Title { get; set; }
+        public string AltText { get; set; }
         public string ImageAlignment { get; set; }
         public string PaddingStyles { get; set; }
     }
