a08adf86965d7a04d73b13d029a589d0e00d6222
Daniel Isaacs <daniel.isaacs@episerver.com>
2023-07-12 13:37:02 +0000

    Update how media types are handled (#936)

--- src/Foundation/Features/Media/FoundationPdfFile.cs
+++ src/Foundation/Features/Media/FoundationPdfFile.cs
@@ -7,11 +7,29 @@ namespace Foundation.Features.Media
     [MediaDescriptor(ExtensionString = "pdf")]
     public class FoundationPdfFile : PdfFile
     {
+        [Display(Name = "Display as Preview?", GroupName = SystemTabNames.Content, Order = 10)]
+        public virtual bool DisplayAsPreview { get; set; }
+
+        [Display(Name = "Show Description (non-Preview Mode)?", 
+            GroupName = SystemTabNames.Content, Order = 20)]
+        public virtual bool ShowDescription { get; set; }
+
+        [Display(Name = "Show Icon (non-Preview Mode)?", GroupName = SystemTabNames.Content, Order = 25)]
+        public virtual bool ShowIcon { get; set; }
+
+        [CultureSpecific]
+        [Display(GroupName = SystemTabNames.Content, Order = 30)]
+        public virtual string Title { get; set; }
+
+        [CultureSpecific]
+        [Display(GroupName = SystemTabNames.Content, Order = 40)]
+        public virtual string Description { get; set; }
+
         [Display(
-            Name = "Height",
+            Name = "Preview Height",
             Description = "The height of PDF preview embed (px)",
             GroupName = SystemTabNames.Content,
-            Order = 100)]
+            Order = 50)]
         public virtual int Height { get; set; }
 
         public override void SetDefaultValues(ContentType contentType)
